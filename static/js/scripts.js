7afa770cee31118a03df67fc196faf5887497ce4
Miker888Gamer <miker888torres@gmail.com>
2026-05-09 17:06:09 -0700

    se agrega el proyecto

---
+++ static/js/scripts.js
@@ -0,0 +1,19 @@
+function saludar() {
+
+
+    //let es una palbra reservada para declarar variables que cambiaran
+    // const es una palabra reservada para valores constantes
+    //var declarar variables pero en versiones nueva js ya no se recomienda
+    
+
+    const humano = document.getElementById("texto-nombre").value;
+    alert(humano);
+
+     //comentario de una linea
+             
+     /*
+             
+     comentario de varias lineas
+             
+     */
+}
